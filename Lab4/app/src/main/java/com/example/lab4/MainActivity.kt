4abeb03bc089c3819aac6d60cf26c0260a33f144
yuhao_huang <t106362525@ntut.org.tw>
2021-07-14 19:22:53 +0800

    create branch

--- Lab4/app/src/main/java/com/example/lab4/MainActivity.kt
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-//test
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
